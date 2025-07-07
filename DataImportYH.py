c0a1371644064cd025683591678246ac0017ad7a
Pibloso <duncan.mccorkell@protonmail.com>
2025-07-07 05:58:54 +0000

    Fully fledged out leverage and removed portfolios that runi from final weighting

--- DataImportYH.py
+++ DataImportYH.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 from datetime import datetime
 
 tickers = ['VT', 'VFMO']
+borrow_rate = 0.092
 start_date = '2018-02-20'
 today = pd.Timestamp.today().normalize()
 last_business_day = pd.bdate_range(end=today, periods=1)[0]
@@ -16,6 +17,7 @@ data = yf.download(tickers, start=start_date, end=end_date, auto_adjust=True)['C
 
 returns = data[tickers].pct_change().dropna()
 returns['CASH'] = (1 + cash_yield)**(1/252)-1
+returns['BORROW'] = (1 - borrow_rate)**(1/252)-1
 
 print(returns)
 plt.plot(returns)
--- PortfolioGrid.py
+++ PortfolioGrid.py
@@ -5,16 +5,12 @@ from DataImportYH import returns
 
 step = 0.01
 weights = []
-borrow = (1-0.092)**(1/252)-1
 
-for w_vt in np.arange(0, 1/.3 + step, step):
-    for w_vmfo in np.arange(0, 1/.3 + step, step):
+for w_vt in np.arange(0, 1/0.3 + step, step):
+    for w_vmfo in np.arange(0, 1/0.3 + step, step):
         w_cash = 1 - w_vt - w_vmfo
-        if w_cash >1 or w_vt + w_vmfo > 1/.3:
+        if w_cash >1 or w_vt + w_vmfo > 1/0.3:
             continue
-        elif w_cash <0:
-            if sum(w_vt * returns['VT']) + sum(w_vmfo * returns['VFMO']) + sum([w_cash * borrow]) <0:
-                continue 
         weights.append((w_vt, w_vmfo, w_cash))
 
 portfolio_log_returns = []
@@ -25,28 +21,17 @@ for w_vt, w_vmfo, w_cash in weights:
         )
     if w_cash >=0:
         port_log_return += w_cash * returns['CASH']
-    else: port_log_return += w_cash * borrow
+    else: port_log_return -= w_cash * returns['BORROW']
     cum_log_wealth = port_log_return.cumsum()
     portfolio_log_returns.append(cum_log_wealth)
 
 print(len(portfolio_log_returns),len(weights))
 
-final_wealths = [p.iloc[-1] for p in portfolio_log_returns]
-wavg = final_wealths / sum(final_wealths)
+final_wealths = np.array([])
+for i, p in enumerate(portfolio_log_returns):
+    if weights[i][2]<0 and np.min(p) <= .3 - 1 / (-weights[i][2] + 1):
+        final_wealths = np.append(final_wealths, 0)
+    else: final_wealths = np.append(final_wealths, p.iloc[-1])
+wavg = final_wealths / np.sum(final_wealths)
 best_weights = np.average(weights, axis=0, weights=wavg)
 print(f"Best Allocation VT:{best_weights[0]}, VFMO: {best_weights[1]}, CASH: {best_weights[2]}")
-
-top_n = 5
-top_indices = np.argsort(final_wealths)[-top_n:]
-
-for idx in top_indices:
-    print(weights[idx], final_wealths[idx])
-
-plt.figure(figsize=(12, 6))
-for idx in top_indices:
-    label = f"VT: {weights[idx][0]:.2f}, VFMO: {weights[idx][1]:.2f}, CASH: {weights[idx][2]:.2f}"
-    plt.plot(portfolio_log_returns[idx],label=label)
-
-plt.grid(True)
-plt.tight_layout()
-plt.show()
